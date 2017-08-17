74784769cc7aa467dc77601c9a64e61f15aae19a
tapioka7 <tkojima703@gmail.com>
2017-08-17 19:44:27 +0900

    Solved ITP1_4_A

---
+++ ITP1/cpp/4a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    
+    cout << a / b << " " << a % b << " " << fixed << setprecision(5) << (double)a / b << endl;
+
+    return 0;
+}
